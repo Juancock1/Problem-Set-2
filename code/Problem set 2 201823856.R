870ef72695d9e0b37d8a5879c7333a88cd2a7960
Juan Camilo Parada Sandoval <104742747+Juancock1@users.noreply.github.com>
2023-09-26 06:54:46 -0500

    Add files via upload

--- code/Problem set 2 201823856.R
+++ code/Problem set 2 201823856.R
@@ -19,4 +19,20 @@ export(x=identification, file = "output/identification.rds")
 
 # Punto 2
 
+# Supongamos que tu data frame se llama "identification"
+
+# Crear la variable "business_type" utilizando ifelse
+identification$business_type <- ifelse(identification$GRUPOS4 == 1, "Agricultura",
+                                       ifelse(identification$GRUPOS4 == 2, "Industria manufacturera",
+                                              ifelse(identification$GRUPOS4 == 3, "Comercio",
+                                                     ifelse(identification$GRUPOS4 == 4, "Servicios", NA))))
+
+
+location <- mutate(location, local = 0)
+location <- mutate(location, local = ifelse(test = P3053>=6 & P3053<8, yes =1, no=0))
+
+identification <- mutate(identification, business_type = case_when(GRUPOS4 == 1 ~ "Agricultura",
+                                                                   GRUPOS4 == 2 ~ "Industria",
+                                                                   GRUPOS4 == 3 ~ "Comercio",
+                                                                   GRUPOS4 == 4 ~ "Servicios"))
 
